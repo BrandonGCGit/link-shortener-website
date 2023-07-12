7c7e6b63a0a4acd1b2dc2e0d41867ce6f53afd30
Brand_Git <brandgc1516@gmail.com>
2023-07-12 17:36:44 -0600

    ClickCopyShortedURL

--- src/components/ComponentsForm.vue
+++ src/components/ComponentsForm.vue
@@ -1,42 +1,58 @@
 <script setup>
 
-let url = '';
-let shortedURL = '';
-
-// Datos de autenticación de la API de Bitly
-const accessToken = 'b60c963a6f3ef72410019d7782758d5a18625283';
-
-// URL que deseas acortar
-const longUrl = 'https://vuejs.org/api/sfc-script-setup.html#top-level-await';
-
-// Configuración de la solicitud HTTP
-// const config = {
-//   headers: {
-//     Authorization: `Bearer ${accessToken}`,
-//     'Content-Type': 'application/json',
-//   },
-// };
-//
-// // Cuerpo de la solicitud HTTP
-// const data = {
-//   long_url: longUrl,
-// };
-//
-// // Realizar la solicitud POST para acortar la URL
-// axios.post('https://api-ssl.bitly.com/v4/shorten', data, config)
-//     .then(response => {
-//       console.log(response.data.link);
-//     })
-//     .catch(error => {
-//       console.error(error);
-//     });
+import {ref} from "vue";
 
 
-const clickCounter = function (){
-  console.log('URL' , url)
-  shortedURL = url
+let longUrl = '';
+const shortedURL = ref("")
+
+const shortedUrls = []
+
+
+// !Methods
+const clickCutOutLink = function () {
+  console.log('URL' , longUrl)
   console.log('SHORTED URL', shortedURL)
+  shortedURL.value = longUrl
+
+  // // Datos de autenticación de la API de Bitly
+  // const accessToken = 'b60c963a6f3ef72410019d7782758d5a18625283';
+  //
+  // // URL que deseas acortar
+  // // const longUrl = 'https://vuejs.org/api/sfc-script-setup.html#top-level-await';
+  //
+  // // Configuración de la solicitud HTTP
+  // const config = {
+  //   headers: {
+  //     Authorization: `Bearer ${accessToken}`,
+  //     'Content-Type': 'application/json',
+  //   },
+  // };
+  //
+  // // Cuerpo de la solicitud HTTP
+  // const data = {
+  //   long_url: longUrl,
+  // };
+  //
+  // // Realizar la solicitud POST para acortar la URL
+  // axios.post('https://api-ssl.bitly.com/v4/shorten', data, config)
+  //     .then(response => {
+  //       shortedURL.value = response.data.link
+  //     })
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+
+
+
 }
+
+const clickCopyShortedUrl = function (){
+  navigator.clipboard.writeText(shortedURL.value)
+}
+
+// !Methods
+
 
 </script>
 
@@ -48,32 +64,40 @@ const clickCounter = function (){
     <!--          Form-->
     <form action="">
       <input
-          v-model="url"
+          v-model="longUrl"
+          aria-label=".form-control-lg example"
           class="form-control form-control-lg rounded-0 shorten-url"
-          type="text"
           placeholder="Enter a link to shorten it"
-          aria-label=".form-control-lg example">
+          type="text">
     </form>
 
     <!--          Form-->
   </div>
   <div class="px-0 col-4 mt-5">
     <button
-        v-on:click="clickCounter"
-        class="m-0 btn btn-lg btn-warning rounded-0 shorten-url ">Shorten URL ></button>
+        class="m-0 btn btn-lg btn-warning rounded-0 shorten-url "
+        v-on:click="clickCutOutLink">Shorten URL >
+    </button>
   </div>
 
   <!--        Link shorted-->
   <div class="col-4 px-0 mt-5 pt-5">
     <input
-        value=""
+        :value="shortedURL"
+        aria-label=".form-control-lg example"
         class="form-control form-control-lg rounded-0 shorten-url"
-        type="text"
         placeholder=""
-        aria-label=".form-control-lg example">
+        type="text">
   </div>
   <div class="px-0 col-4 mt-5 pt-5">
-    <button class="m-0 btn btn-lg btn-warning rounded-0 shorten-url "><span class="material-symbols-outlined">file_copy</span></button>
+    <button
+        v-on:click="clickCopyShortedUrl"
+        class="m-0 btn btn-lg btn-warning rounded-0 shorten-url ">
+      <span
+        class="material-symbols-outlined">
+        file_copy
+      </span>
+    </button>
   </div>
 
   <!--        Link shorted-->
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './assets/main.css'
+// import 'tippy.js/dist/tippy.css'; // optional for styling
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import '../node_modules/axios/dist/axios'
-
 
 
 createApp(App).mount('#app')
